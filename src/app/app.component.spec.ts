96d759c6a63726f140704fb4743ad59ed243d731
besscerule <besscerule@gmail.com>
2024-10-18 15:10:46 +0100

    simple-battle-set-up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, BrowserAnimationsModule, ReactiveFormsModule],
     }).compileComponents();
   });
 
@@ -12,18 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'battle-simulator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('battle-simulator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, battle-simulator');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BattleDetailsComponent } from "./battle-details/battle-details.component";
-import { BattleResultComponent } from "./battle-result/battle-result.component";
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BattleDetailsComponent, BattleResultComponent],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'battle-simulator';
+
+  teamASizeFormControl  = new FormControl(0);
+  teamBSizeFormControl  = new FormControl(0);
+  formattedResult = ""
+
+  simulateBattle() {
+    const teamASize = this.teamASizeFormControl.value as number
+    const teamBSize = this.teamBSizeFormControl.value as number
+
+    const result = this.calculateSimulation(teamASize, teamBSize)
+
+    this.formattedResult = (result*100).toFixed(2)
+  }
+
+  calculateSimulation(teamASize: number, teamBSize: number) {
+    // this is where you actually do the calculation
+    return teamASize/(teamASize + teamBSize)
+  }
 }
